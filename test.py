50ca8233ad8c80d8ea2bee1637e7bc7d2093a0f6
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-24 06:09:09 +0100

    Updated

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ print(numero)
 #print(numero2)   Línea desactivada
 print(numero, numero2)
 
-#Comentario en Python, esta parte solo es para las personas.
 # Este bucle for itera en sentido decreciente
 for i in range(10,2,-1):
     print(i)
