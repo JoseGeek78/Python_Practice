ba975e8e8c2f667838911772ccb32e6cc977d459
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-24 06:12:57 +0100

    Updated

--- test.py
+++ test.py
@@ -9,3 +9,10 @@ print(numero, numero2)
 # Este bucle for itera en sentido decreciente
 for i in range(10,2,-1):
     print(i)
+    
+"""
+# Este código está anulado y no se ejecuta
+for i in range(10,0,-1)
+  print(i)
+
+"""
