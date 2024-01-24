305730f53995915ae81b53f28c5e9e8e579ba867
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-24 06:05:08 +0100

    Updated

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ numero2 = 202
 numero = 111
 
 print(numero)
-print(numero2)
+#print(numero2)   Línea desactivada
 print(numero, numero2)
 
 #Comentario en Python, esta parte solo es para las personas.
