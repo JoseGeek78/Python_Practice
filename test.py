d1df7ca2ac9209e461718890eb737abe7bd0713f
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-25 19:49:25 +0100

    Updated

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-numero = 10
-numero2 = 202
-numero = 111
-
-print(numero)
-#print(numero2)   Línea desactivada
-print(numero, numero2)
-
 # Este bucle for itera en sentido decreciente
 for i in range(10,2,-1):
     print(i)
