20b196014b10cd60ef165fb8238d1751042f3072
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-25 20:01:47 +0100

    Updated

--- test.py
+++ test.py
@@ -9,9 +9,15 @@ for i in range(10,0,-1)
   print(i)
 
 """
-def get_full_name(first_name, last_name):
+def get_full_name(first_name: str, last_name: str):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
 
 
-print(get_full_name("john", "doe"))
+print(get_full_name("papa", "hop"))
+
+def get_name_with_age(name: str, age: int):
+    name_with_age = name.title() + " is this old: " + str(age)
+    return name_with_age
+  
+print(get_name_with_age("jose", 45))
